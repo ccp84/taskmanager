d4bbba1b551e048927fa5515e638d1a69aad7bb7
ccp84 <ccp84@icloud.com>
2023-01-20 13:56:09 +0000

    py>add new templates to routes file

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -6,3 +6,8 @@ from taskmanager.models import Task, Category
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
