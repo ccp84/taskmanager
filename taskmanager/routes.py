f8dc2ffa7938d7d9d03e9e8e93574970f89208c5
ccp84 <ccp84@icloud.com>
2023-01-31 15:42:57 +0000

    py>add sort on category table to routes

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -8,9 +8,10 @@ def home():
     return render_template("tasks.html")
 
 
-@app.route("/categories", methods=["GET", "POST"])
+@app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
 
 
 @app.route("/add_category", methods=["GET", "POST"])
